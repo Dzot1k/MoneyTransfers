7c2a9758ac41a2a5c798a5e591e5704a54b8b441
Dzot1k <dzot988@mail.ru>
2022-03-12 13:27:20 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val amount = 1_000_000
+    val percentageCommission = 0.0075
+    val minAmountCommission = 3500
+    val amountCommission = if (amount * percentageCommission > minAmountCommission) amount * percentageCommission else minAmountCommission
+    println("Комиссия за перевод составит: $amountCommission копеек")
+}
